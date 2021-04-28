1fbd66a0d86556fa2e6c431cff812d54b87d0136
Sh09507 <71281402+Sh09507@users.noreply.github.com>
2021-04-28 04:15:28 -0400

    Add files via upload

--- topic.php
+++ topic.php
@@ -14,13 +14,44 @@
 
 <body>
 	<header>
-	<h1>Welcome to the Discussion!</h1>
 		<nav>
 			<?php
 				require "nav.php";
 			?>
 		</nav>
+		<main>
+			<?php
+			$t = $GET_['t'];
+			require_once 'database.php';
+			try {
+				$myDBconnection = new PDO("mysql:host=$HOST_NAME;dbname=$DATABASE_NAME", $USERNAME, $PASSWORD);
+			} catch (PDOException $e) {
+				$error_message = $e->getMessage();
+				print $error_message . "<br>";
+			}
+			try{
+				$query = "SELECT ID, title, user_name, body, image FROM discussion_topics WHERE ID = :id;";
+				$dbquery = $myDBconnection -> prepare($query);
+				$dbquery = $bindvalue(':id', $t);
+				$dbquery -> execute();
+				$result = $dbquery -> fetch();
+			} catch (PDOException $e) {
+						$error_message = $e -> getMessage();
+						echo $error_message . "<br>";
+				}
+					if($results != ""){
+						foreach ($results as &$arr) {
+						?>
+							<h1><?php echo $arr['title']; ?></h1>
+							<img src="images/<?php echo $arr['image']; ?>" alt="uploaded image">
+							<h3><?php echo $arr['body'];?></h3>
+							<p><?php echo "This post was brought to you by " . $arr['user_name'];?> </p> 
+						<?php }
+					}else {
+						echo "<p>The topic you are looking for does not exist.</p>";
+					}
+			?>
+		</main>
 	</header>	
 </body>
-
 </html>
