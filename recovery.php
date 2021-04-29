b27171a12e8e5475c67e3b5e35184c95a22134e0
Sh09507 <71281402+Sh09507@users.noreply.github.com>
2021-04-28 22:47:26 -0400

    Add files via upload

--- recovery.php
+++ recovery.php
@@ -28,6 +28,7 @@ if ($pwdrec == False) {
 
 <?php
 //Connect to DB
+	// lines 32-38 was code referenced from Hawkins Web programming Lab 14 index.php
 	require_once 'database.php'; 
 	try {
 		$myDBconnection = new PDO("mysql:host=$HOST_NAME;dbname=$DATABASE_NAME", $USERNAME, $PASSWORD);
@@ -37,6 +38,7 @@ if ($pwdrec == False) {
 	}
 		
 	//sanitize function (to clean up malicious data)
+	//sanatization is referenced from my web programming Assignment 5 that Hawkins helped with, and the this assignment took reference code from web programming lab 16 with htmlentities and strip_tags/stripcslashes
 	function sani($bad){
 		$bad = stripslashes($bad);
 		$bad = strip_tags($bad);
@@ -69,6 +71,7 @@ if ($pwdrec == False) {
 					//do all the sanitized variables still have a value?
 					if($password != "" ) {
 					//Does the username match the data in the table? 
+						//  Line 75 Was shown in class this semester by Thackston
 						$password = password_hash($password, PASSWORD_DEFAULT);
 						$query = 'UPDATE users SET password = :password WHERE user_name = :username;';
 						$statement = $myDBconnection -> prepare($query);
@@ -76,6 +79,7 @@ if ($pwdrec == False) {
 						$statement -> bindValue(':password', $password);
 						$statement -> execute();
 						echo "Password has been updated. Please try Logging in.";
+						//  Line 82 Was shown in class this semester by Thackston
 						setcookie('Pwdcookie', $token, time() - 3600, "/");
 						require_once "logging.php";
 						auditlog($myDBconnection,"User Password Changed", 1, $pwdrecUser, $password, NULL, NULL);
